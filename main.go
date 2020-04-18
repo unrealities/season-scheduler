3cb0a05fee66f5ee392284b9e465cfcbcf087ccd
Tom Szymanski <szymanski.tom@gmail.com>
2020-04-18 14:30:08 -0600

    basic next playable date function

--- main.go
+++ main.go
@@ -127,7 +127,22 @@ func (g game) prettyPrint(c config) string {
 // and gives the next available date that the team can play
 func (t team) nextPlayableDate(date time.Time, doubleHeaders bool, games schedule) time.Time {
 	// TODO: Handle doubleHeaders
-	// TODO: Games should be in date order, so you can go to a team's last date and
-	// then check that date and give the next available date
-	return date
+	if doubleHeaders {
+		return date
+	}
+
+	// base case. If a team has no games, then their first game should be on the start of the season
+	if len(games) == 0 {
+		return date
+	}
+
+	// get the most recently played game (which should be the last game in the schedule slice)
+	mostRecentGame := games[len(games)-1]
+	mostRecentDate := mostRecentGame.Time
+
+	// return the next available date
+	// TODO: account for league holidays
+	// TODO: account for too many consecutive games
+	// TODO: allow for travel days
+	return mostRecentDate.AddDate(0, 0, 1)
 }
